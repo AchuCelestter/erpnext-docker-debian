671b4b82df163ad63eb567a0b5c6213ca6bee7ea
AchuCelestter <achu.celestter@gmail.com>
2026-04-19 01:30:56 +0100

    updated entry point

--- deploy/shared/entrypoint.sh
+++ deploy/shared/entrypoint.sh
@@ -5,7 +5,7 @@ echo "-> Set ownership of sites folder"
 chown frappe:frappe /home/frappe/bench/sites
 
 echo "-> Linking assets"
-su frappe -c "ln -sf /home/frappe/bench/built_sites/assets /home/frappe/bench/sites/assets"
+su frappe -c "rm -rf /home/frappe/bench/sites/assets && ln -sf /home/frappe/bench/built_sites/assets /home/frappe/bench/sites/assets"
 su frappe -c "ln -sf /home/frappe/bench/built_sites/apps.json /home/frappe/bench/sites/apps.json"
 su frappe -c "ln -sf /home/frappe/bench/built_sites/apps.txt /home/frappe/bench/sites/apps.txt"
 
